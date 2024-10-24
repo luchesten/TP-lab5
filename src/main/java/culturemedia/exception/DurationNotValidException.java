a4e277bb398b2d7967b1e5be2f22dbf9cd33dc9f
steven aguirre giraldo <120338753+luchesten@users.noreply.github.com>
2024-10-24 00:21:24 -0500

    Adding Service-Structure and service cases 2

--- src/main/java/culturemedia/exception/DurationNotValidException.java
+++ src/main/java/culturemedia/exception/DurationNotValidException.java
@@ -1,7 +1,9 @@
 package culturemedia.exception;
 
+import java.text.MessageFormat;
 public class DurationNotValidException extends CultureMediaException {
     public DurationNotValidException(String title,Double duration) {
-        super("The duration of the video:" + title +",it's too short : " + duration);
+        super(MessageFormat.format(
+                "The duration of the video {0} is too short : {1}",title,duration));
     }
 }
--- src/main/java/culturemedia/service/impl/CulturemediaServiceImpl.java
+++ src/main/java/culturemedia/service/impl/CulturemediaServiceImpl.java
@@ -7,6 +7,7 @@ import culturemedia.repository.VideoRepository;
 import culturemedia.repository.ViewsRepository;
 import culturemedia.service.CulturemediaService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CulturemediaServiceImpl implements CulturemediaService {
@@ -22,20 +23,50 @@ public class CulturemediaServiceImpl implements CulturemediaService {
     @Override
     public List<Video> findAll() throws VideoNotFoundException {
         List<Video> videos = videoRepository.findAll();
-        if(videos.isEmpty()){
-            throw new VideoNotFoundException();}
-        else{
+        if (videos.isEmpty()) {
+            throw new VideoNotFoundException();
+        } else {
             return videos;
         }
     }
+
     @Override
-    public Video save(Video video){
+    public Video save(Video video) {
         videoRepository.save(video);
         return video;
     }
+
     @Override
-    public View save(View view){
+    public View save(View view) {
         viewsRepository.save(view);
         return view;
+    }
+
+    public List<Video> findByTitle(String title) throws VideoNotFoundException {
+        List<Video> videos = videoRepository.findAll();
+        List<Video> matchingVideos = new ArrayList<>();
+        for (Video video : videos) {
+            if (video.title().toLowerCase().contains(title.toLowerCase())) {
+                matchingVideos.add(video);
+            }
+        }
+        if(matchingVideos.isEmpty()) {
+            throw new VideoNotFoundException(title);
+        }
+        return matchingVideos;
+    }
+
+    public List<Video> findByDuration(Double duration) throws VideoNotFoundException {
+        List<Video> videos = videoRepository.findAll();
+        List<Video> savedVideos = new ArrayList<>();
+        for (Video video : videos) {
+            if (video.duration().equals(duration)) {
+                savedVideos.add(video);
+            }
+        }
+        if (savedVideos.isEmpty()){
+            throw new VideoNotFoundException();
+        }
+        return savedVideos;
     }
 }
