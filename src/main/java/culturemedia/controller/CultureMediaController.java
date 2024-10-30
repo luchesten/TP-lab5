a5e19879e39809486d8586fa03ac0fb438ce97f0
steven aguirre giraldo <120338753+luchesten@users.noreply.github.com>
2024-10-30 18:44:13 -0500

    Adding Service-Structure and service cases 2

---
+++ src/main/java/culturemedia/controller/CultureMediaController.java
@@ -0,0 +1,28 @@
+package culturoteca.controller;
+
+package culturemedia.controller;
+
+import java.util.*;
+
+import culturoteca.exception.VideoNotFoundException;
+import culturoteca.model.Video;
+import culturoteca.service.impl.CultureMediaServiceImpl;
+
+public class CultureMediaController {
+
+    private final CultureMediaServiceImpl cultureMediaService;
+
+
+    public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+        this.cultureMediaService = cultureMediaService;
+    }
+
+
+    public List<Video> find_allVideos() throws VideoNotFoundException {
+        List<Video> videos = null;
+        videos = cultureMediaService.findAll();
+        return videos;
+    }
+
+
+}
